da947642c94023a0c4bdc3f158d3b9a594564122
Ben Neely <ben@bloc.io>
2015-05-28 20:25:17 -0600

    Complete Building Blocstagram Checkpoint

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Ben Neely on 5/28/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
